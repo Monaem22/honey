b4be439d023d39ea79fea1f3d69ae06ba336d7fb
Monaem22 <manemosama@gmail.com>
2024-09-27 17:26:06 +0300

    all files

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-366.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e4231c18' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-93dfe43f' );
+});
+ });
